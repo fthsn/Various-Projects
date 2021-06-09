b40a779cc5e06228c60d52f8c2b2b71e78be2785
fthsn <49636833+fthsn@users.noreply.github.com>
2021-06-09 16:24:07 +0300

    Update DetectOutliers.R

--- Mix/DetectOutliers.R
+++ Mix/DetectOutliers.R
@@ -1,31 +1,117 @@
+####################################################################
+## Function FthsnGetOutliers
+########################################
+########################################
+FthsnGetOutliers = function(data='mydata',vl='',plimit=0.0001) {
+  
+  # data = 'mydata'
+  # ii = 'Crp'
 
-FthsnGetOutliers = function(data='',vl='') {
-data = get(data)
-
-for (ii in vl) {
-var = ii
-print(ii)
-mean(data[,var])
-frml = as.formula(paste(var,' ~ 1'))
-mod = lm(frml,data=data)
-cooksd = cooks.distance(mod);plot(cooksd);text(cooksd,row.names(cooksd))
-influential <- as.numeric(names(cooksd)[(cooksd > (4/nrow(data)))])
-outliers = as.numeric(names(car::outlierTest(mod)$bonf.p))
-detected = intersect(influential,outliers)
-x = data[-detected,var]
-
-for (i in detected) {
-  a = data[i,var]  
-  w = which(abs(x-a)==min(abs(x-a)))
-  data[i,var]  =  mean(x[w])
-  print(paste(i,a,mean(x[w])))
-
-}
-}
-return(data.table(data))}
-
-say = colnames(Merged2[,sapply(Merged2,is.numeric)])
+  data = data.frame(get(data))
+  
+  print('########### 1st Round ')
+  for (ii in vl) {
+    var = ii
+    # print(ii)
+    frml = as.formula(paste(var,' ~ 1'))
+    mod = lm(frml,data=data)
+    cooksd = cooks.distance(mod);
+    # text(cooksd,row.names(cooksd))
+    influential <- as.numeric(names(cooksd)[(cooksd > (4/nrow(data)))])
+    
+    oudt = data.table(car::outlierTest(mod,cutoff=plimit)$bonf.p)
+    oudt$names = names(car::outlierTest(mod,cutoff=plimit)$bonf.p)
+    oudt = oudt[V1<plimit]
+    # if (nrow(oudt)==0) {oudt=0}
+    
+    outliers = as.numeric(oudt$names)
+    detected = intersect(influential,outliers)
+    if(length(detected)>0) {plot(cooksd,main = var);}
+    
+    
+    x = data[-detected,var]
+    if(length(detected)>0) { 
+    print(paste(ii,'(',paste(data[detected,var],collapse = ', '),')'))
+    }
+    for (i in detected) {
+      a = data[i,var]  
+      w = which(abs(x-a)==min(abs(x-a),na.rm = TRUE))
+      data[i,var]  =  mean(x[w],na.rm = TRUE)
+      # print(paste(i,a,mean(x[w])))
+      
+    }
+  }
+  print('########### 2nd Round ')
+  # plimit = 0.01
+  for (ii in vl) {
+    var = ii
+    # print(ii)
+    frml = as.formula(paste(var,' ~ 1'))
+    mod = lm(frml,data=data)
+    cooksd = cooks.distance(mod);
+    # text(cooksd,row.names(cooksd))
+    influential <- as.numeric(names(cooksd)[(cooksd > (4/nrow(data)))])
+    
+    oudt = data.table(car::outlierTest(mod,cutoff=plimit)$bonf.p)
+    oudt$names = names(car::outlierTest(mod,cutoff=plimit)$bonf.p)
+    oudt = oudt[V1<plimit]
+    # if (nrow(oudt)==0) {oudt=0}
+    
+    outliers = as.numeric(oudt$names)
+    detected = intersect(influential,outliers)
+    if(length(detected)>0) {plot(cooksd,main = var);}
+    
+    
+    x = data[-detected,var]
+    if(length(detected)>0) { 
+      print(paste(ii,'(',paste(data[detected,var],collapse = ', '),')'))
+    }
+    for (i in detected) {
+      a = data[i,var]  
+      w = which(abs(x-a)==min(abs(x-a),na.rm = TRUE))
+      data[i,var]  =  mean(x[w],na.rm = TRUE)
+      # print(paste(i,a,mean(x[w])))
+      
+    }
+  }
+  
+  print('########### 3th Round ')
+  # plimit = 0.01
+  for (ii in vl) {
+    var = ii
+    # print(ii)
+    frml = as.formula(paste(var,' ~ 1'))
+    mod = lm(frml,data=data)
+    cooksd = cooks.distance(mod);
+    # text(cooksd,row.names(cooksd))
+    influential <- as.numeric(names(cooksd)[(cooksd > (4/nrow(data)))])
+    
+    oudt = data.table(car::outlierTest(mod,cutoff=plimit)$bonf.p)
+    oudt$names = names(car::outlierTest(mod,cutoff=plimit)$bonf.p)
+    oudt = oudt[V1<plimit]
+    # if (nrow(oudt)==0) {oudt=0}
+    
+    outliers = as.numeric(oudt$names)
+    detected = intersect(influential,outliers)
+    if(length(detected)>0) {plot(cooksd,main = var);}
+    
+    
+    x = data[-detected,var]
+    if(length(detected)>0) { 
+      print(paste(ii,'(',paste(data[detected,var],collapse = ', '),')'))
+    }
+    for (i in detected) {
+      a = data[i,var]  
+      w = which(abs(x-a)==min(abs(x-a),na.rm = TRUE))
+      data[i,var]  =  mean(x[w],na.rm = TRUE)
+      # print(paste(i,a,mean(x[w])))
+      
+    }
+  }
+  
+  
+  
+  
  
-xx = FthsnGetOutliers('Merged2',"Kadrolu_Ogrt_Oran")
-
- 
+  
+  return(data.table(data))}
